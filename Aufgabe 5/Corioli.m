5e4ec7eb5c78e748ead88b75d1a75a2efb9f300c
Erik <140516404+Slice00@users.noreply.github.com>
2025-03-12 15:53:12 +0100

    Add files via upload

--- Aufgabe 5/Corioli.m
+++ Aufgabe 5/Corioli.m
@@ -1,11 +1,19 @@
+clear, close all;
+
 robot = loadrobot('kukaIiwa14', 'DataFormat', 'column'); % Load Franka Panda robot
 
 q = [0.3; -0.5; 0.3; -0.1; 0.7; -0.4; 0.5]; 
-q_dot = [1.4834; 1.4834;1.7452;1.3089;2.2688 ;2.356;2.356]; 
+
+q_dot1 = [0.3; 0.3;0.3;0.3;0.3 ;0.3;0.3]; 
+q_dot2 = [0.6; 0.6;0.6;0.6;0.6 ;0.6;0.6]; 
+q_dot3 = [0.9; 0.9;0.9;0.9;0.9 ;0.9;0.9]; 
+
+q_dot_array = [q_dot1, q_dot2, q_dot3];
+
+
 q_ddot = [0.5; 0.2; -0.3; 0.1; 0.4; -0.2; 0]; 
 
 J = geometricJacobian(robot, q, 'iiwa_link_ee'); % Compute Jacobian for end-effector
-end_effector_velocity = J * q_dot;
 
 % Compute Mass-Inertia Matrix
 M = massMatrix(robot, q);
@@ -13,12 +21,26 @@ M = massMatrix(robot, q);
 % Compute Euler Forces
 euler_forces = M * q_ddot;
 
-disp('End-Effector Velocity (Linear + Angular):')
-disp(end_effector_velocity)
+disp('Euler Forces:'), disp(euler_forces);
 
-coriolis_forces = velocityProduct(robot, q, q_dot);
+for i = 1:3
 
-disp('Coriolis Forces')
-disp(coriolis_forces)
+end_effector_velocity = J * q_dot_array(:,i);
+coriolis_forces = velocityProduct(robot, q, q_dot_array(:,i));
 
-disp('Euler Forces:'), disp(euler_forces)
+Coriolis_Vec(:,i) = coriolis_forces;
+
+
+
+end
+
+disp('Coriolis Forces v1 bis 3')
+disp(Coriolis_Vec)
+
+% disp('End-Effector Velocity (Linear + Angular):')
+% disp(end_effector_velocity)
+
+
+
+
+
